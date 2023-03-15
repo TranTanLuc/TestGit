b867e79394ceca51fea60a5c32e65f035718a458
Tran Tan Luc <luc972000@gmail.com>
2023-03-16 00:25:34 +0700

    C2

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,8 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 1;
+        int y = 2;
+        System.out.println("Day la C2");
     }
 }
