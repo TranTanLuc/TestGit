c9f0ad42d7321d42039fbb7b45bd35da9ef1daf4
Tran Tan Luc <luc972000@gmail.com>
2023-03-16 00:29:33 +0700

    C4

--- src/main/Demo.java
+++ src/main/Demo.java
@@ -1,9 +1,7 @@
 package main;
 
-public class Main {
+public class Demo {
     public static void main(String[] args) {
-        int x = 4;
-        int y = 5;
         System.out.println("Day la C4");
     }
 }
